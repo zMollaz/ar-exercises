a5b4d62270025b16cee77c70ebf9441a5b281734
A E <aelmollaz@gmail.com>
2022-02-01 16:25:32 +0000

    Exercise 1 done, Create

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,33 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+class Store < ActiveRecord::Base
+end
+
+#creates new recors in the stores table
+Burnaby = Store.create(
+  name: 'Burnaby',
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+Richmond = Store.create(
+  name: 'Richmond',
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+#Trying different syntax for create
+Gastown = Store.new
+Gastown.name = 'Gastown'
+Gastown.annual_revenue = 190000
+Gastown.mens_apparel = true
+Gastown.womens_apparel = false
+Gastown.save
+
+puts Store.count
