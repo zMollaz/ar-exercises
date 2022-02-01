ddc8818dc494c314f32a4cbad5b2f7901ec58b9b
A E <aelmollaz@gmail.com>
2022-02-01 18:29:21 +0000

    Exercise 6 done, Associations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,35 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Hisham", 
+  last_name: "Abbas", 
+  hourly_rate: 80
+)
+@store1.employees.create(
+  first_name: "Mostafa", 
+  last_name: "Ammar", 
+  hourly_rate: 45
+)
+@store2.employees.create(
+  first_name: "Raafat", 
+  last_name: "Elhagan", 
+  hourly_rate: 120
+)
+@store2.employees.create(
+  first_name: "Sayed", 
+  last_name: "Zayan", 
+  hourly_rate: 100
+)
+@store2.employees.create(
+  first_name: "Ibrahim", 
+  last_name: "Elkhateeb", 
+  hourly_rate: 50
+)
+
+puts Employee.all.inspect
